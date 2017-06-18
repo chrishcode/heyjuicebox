91d24499099e35554dffc87901424ae8578c5adc
Christopher Wohlfarth <chris.wohlfarth@gmail.com>
2017-06-18 03:22:30 +0200

    first commit - setup basic style components and dashboard authentication

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+    <div style="display: flex; align-items: center;">
+        <div class="col-md-6">
+            <div class="panel panel-default" style="min-height: 550px;">
+                <div class="panel-heading">AMAZON PRODUCT SEARCH</div>
+
+                <div class="panel-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-default" style="min-height: 550px;">
+                <div class="panel-heading">JUICE BOXES</div>
+
+                <div class="panel-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+@endsection
