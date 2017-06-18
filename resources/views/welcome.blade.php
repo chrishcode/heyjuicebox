decd1a8a49c196ba957b5a117f48bd6034031200
Christopher Wohlfarth <chris.wohlfarth@gmail.com>
2017-06-18 11:40:31 +0200

    markup and style subscription box

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,65 @@
 
     <div class="body-section">
         <h3>EARLIER BOXES THIS YEAR</h3><br>
-        
+        <div class="subsciption-box">
+            <div class="subsciption-box-info">
+                <h4>MAY'S BOX - JUICE FROM THE RAW</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor lorem id sem condimentum, eu finibus sapien bibendum. Etiam euismod rhoncus nisl, et viverra lectus commodo sit amet.</p><br>
+                <ul>
+                    <li><h3>DETOX GREENS</h3></li>
+                    <li><h3>COCONUT FUSION</h3></li>
+                    <li><h3>PINEAPPLE MINT</h3></li>
+                    <li><h3>ROOT VEGGIES</h3></li>
+                    <li><h3>SPICY LEMONADE</h3></li>
+                    <li><h3>SWEET GREENS</h3></li>
+                </ul>
+            </div>
+            <div class="subsciption-box-images">
+                <ul>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>   
+                </ul>
+            </div>
+        </div>
+        <div class="subsciption-box">
+            <div class="subsciption-box-info">
+                <h4>APRIL'S BOX - JUICE FROM THE RAW</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor lorem id sem condimentum, eu finibus sapien bibendum. Etiam euismod rhoncus nisl, et viverra lectus commodo sit amet.</p><br>
+                <ul>
+                    <li><h3>DETOX GREENS</h3></li>
+                    <li><h3>COCONUT FUSION</h3></li>
+                    <li><h3>PINEAPPLE MINT</h3></li>
+                    <li><h3>ROOT VEGGIES</h3></li>
+                    <li><h3>SPICY LEMONADE</h3></li>
+                    <li><h3>SWEET GREENS</h3></li>
+                </ul>
+            </div>
+            <div class="subsciption-box-images">
+                <ul>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>
+                    <li style="background-image: url(https://images-na.ssl-images-amazon.com/images/I/81KKiiVaaoL._SL1500_.jpg)"></li>   
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="body-section">
+        <h3>SIGN UP, IT'S FREE</h3><br>
+        <form class="signup-form">
+            <input id="email" type="email" class="form-control" name="email" placeholder="example@email.com">
+            <button type="submit" class="btn btn-primary">
+                SIGN UP
+            </button>
+        </form>
     </div>
 @endsection
